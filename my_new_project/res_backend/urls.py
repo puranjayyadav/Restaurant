133d6e6dda9af413f4e4d46fcbd33df4ec450fcd
Puranjay Yadav <puranjayrocks4@gmail.com>
2025-03-11 14:00:02 -0400

    MVP Half Done

--- my_new_project/res_backend/urls.py
+++ my_new_project/res_backend/urls.py
@@ -5,4 +5,5 @@ from .views import verify_token,get_trips
 urlpatterns = [
     path('verify-token/', verify_token, name='verify_token'),
     path('get-trips/', get_trips, name='get_trips'),
+    #path('create_session/', create_session,name='create_session')
 ]
